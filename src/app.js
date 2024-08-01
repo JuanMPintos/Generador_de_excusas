0fca7e9d1d27aac14e17bb14a75820e0f12e9bf5
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-08-01 08:51:55 +0000

    lineamiento de codigo

--- src/app.js
+++ src/app.js
@@ -1,29 +1,23 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 function generadorDeExcusas() {
-  const quienes = ["El perro", "El gato", "El conejo", "El loro"];
-  const acciones = ["Se comio", "Me rompio", "Le hizo caca", "Le hizo pis"];
-  const objetos = ["la tarea", "las fotos", "el proyecto", "los archivos"];
-  const lugares = [
-    "en la sala",
-    "en el parque",
-    "en la cocina",
-    "en el dormitorio"
-  ];
+  const QUIENES = ["El perro", "El gato", "El conejo", "El loro"];
+  const ACCIONES = ["Se comio", "Me rompio", "Le hizo caca", "Le hizo pis"];
+  const OBJETOS = ["la tarea", "las fotos", "el proyecto", "los archivos"];
+  const LUGARES = ["en la sala", "en el parque", "en la cocina", "en el dormitorio"];
 
-  const quien = quienes[Math.floor(Math.random() * quienes.length)];
-  const accion = acciones[Math.floor(Math.random() * acciones.length)];
-  const objeto = objetos[Math.floor(Math.random() * objetos.length)];
-  const lugar = lugares[Math.floor(Math.random() * lugares.length)];
+  const QUIEN = QUIENES[Math.floor(Math.random() * QUIENES.length)];
+  const ACCION = ACCIONES[Math.floor(Math.random() * ACCIONES.length)];
+  const OBJETO = OBJETOS[Math.floor(Math.random() * OBJETOS.length)];
+  const LUGAR = LUGARES[Math.floor(Math.random() * LUGARES.length)];
 
-  return `${quien} ${accion} ${objeto} ${lugar}.`;
+  return `${QUIEN} ${ACCION} ${OBJETO} ${LUGAR}`;
 }
 
 window.onload = function() {
   document.getElementById("excuse").innerHTML = generadorDeExcusas();
-};
+}
